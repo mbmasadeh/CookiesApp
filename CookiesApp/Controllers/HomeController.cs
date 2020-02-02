52f675ea287ab7b7f51edbdf8de042a89a4f2b17
Mohad B.Masadeh <mbmasadeh@outlook.com>
2020-02-02 10:37:52 +0300

    Saving

--- CookiesApp/Controllers/HomeController.cs
+++ CookiesApp/Controllers/HomeController.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CookiesApp.ViewModels
+namespace CookiesApp.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
